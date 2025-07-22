d212ba9abd836189d5b6d42936bfdb1b2151036e
Ariel <84594293+larielol@users.noreply.github.com>
2025-07-22 09:55:48 -0300

    Add files via upload

---
+++ Prova01/scripts/script.js
@@ -0,0 +1,66 @@
+let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+
+function adicionaAoCarrinho(nomeProduto, precoProduto) {
+    const produto = { nome: nomeProduto, preco: precoProduto };
+    carrinho.push(produto);
+    atualizaContagemCarrinho();
+    salvarCarrinho();
+    alert(`O produto ${nomeProduto} foi adicionado ao seu carrinho.`);
+}
+
+function atualizaContagemCarrinho() {
+    document.getElementById('carrinho-contagem').textContent = carrinho.length;
+}
+
+function salvarCarrinho() {
+    sessionStorage.setItem('carrinho', JSON.stringify(carrinho));
+}
+
+function carregaCarrinho() {
+    carrinho = JSON.parse(sessionStorage.getItem('carrinho')) || [];
+    atualizaContagemCarrinho();
+    mostrarItensCarrinho();
+}
+
+function mostrarItensCarrinho() {
+    const containerCarrinho = document.getElementById('carrinho-container');
+    const totalCarrinho = document.getElementById('carrinho-total');
+    containerCarrinho.innerHTML = '';
+    let total = 0;
+
+    carrinho.forEach((produto, indice) => {
+        const itemCarrinho = document.createElement('div');
+        itemCarrinho.classList.add('carrinho-item');
+        
+        itemCarrinho.innerHTML = `
+            <img src="./img/${produto.nome}.jpg" alt="${produto.nome}">
+            <div class="carrinho-item-detalhes">
+                <h3>${produto.nome}</h3>
+                <p>${produto.preco.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</p>
+            </div>
+            <button onclick="removerItemCarrinho(${indice})">Remover</button>
+        `;
+
+        containerCarrinho.appendChild(itemCarrinho);
+        total += produto.preco;
+    });
+
+    totalCarrinho.textContent = total.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+}
+
+function removerItemCarrinho(indice) {
+    carrinho.splice(indice, 1);
+    atualizaContagemCarrinho();
+    salvarCarrinho();
+    mostrarItensCarrinho();
+}
+
+function limpaCarrinho() {
+    carrinho = [];
+    atualizaContagemCarrinho();
+    salvarCarrinho();
+    mostrarItensCarrinho();
+}
+
+// Linha para carregar o carrinho ao carregar a página
+window.onload = carregaCarrinho;
